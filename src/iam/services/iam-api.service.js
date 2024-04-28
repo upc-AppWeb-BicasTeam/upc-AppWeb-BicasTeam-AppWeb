90fe5d76ac5767817f3592d53f4846fc9755e54a
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-04-27 22:08:05 -0500

    fix: commit

--- src/iam/services/iam-api.service.js
+++ src/iam/services/iam-api.service.js
@@ -4,6 +4,9 @@ export class IamApiService{
     createUser(jsonUser){
         return http.post('/users',jsonUser)
     }
+    getAllUsers(){
+        return http.get('/users')
+    }
     findUserWithEmailAndPassword(email,password){
         return http.get(`/users?email=${email}&password=${password}`)
     }
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,11 @@ import loginComponent from "../public/pages/login.component.vue";
 import registerComponent from "../public/pages/register.component.vue";
 import registerInformationComponent from "../public/pages/register-information.component.vue";
 import registerInformationSuccessfullyComponent from "../public/pages/register-information-successfully.component.vue";
+<<<<<<< HEAD
 import homeBusinessmanComponent from "../profiles-managment/components/home-businessman.component.vue";
 import homeDriverComponent from "../profiles-managment/components/home-driver.component.vue";
+=======
+>>>>>>> parent of f71b192 (fix(components ubication): fix componets in profiles managment bounded context)
 
 const router  = createRouter({
     history: createWebHistory(),
@@ -13,11 +16,15 @@ const router  = createRouter({
         {path:'/register', component:registerComponent},
         {path:'/register/:type', component: registerInformationComponent},
         {path:'/register/successfully', component:registerInformationSuccessfullyComponent},
+<<<<<<< HEAD
         { path: '/home-businessman',    component: homeBusinessmanComponent},
         { path: '/home-carrier',    component: homeDriverComponent},
         {path:'/',redirect:'/login'},
 
 
+=======
+        {path:'/',redirect:'/login'}
+>>>>>>> parent of f71b192 (fix(components ubication): fix componets in profiles managment bounded context)
     ]
 });
 export default router;
