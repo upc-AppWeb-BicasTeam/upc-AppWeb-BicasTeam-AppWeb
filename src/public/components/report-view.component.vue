e828f4c6e64178e1808a1b7b0bd497fe67c79414
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-04-28 18:46:58 -0500

    fix(report-view.component): flex
    fix flex for the component

--- src/public/components/report-view.component.vue
+++ src/public/components/report-view.component.vue
@@ -27,12 +27,12 @@ export default {
 </script>
 
 <template>
-    <div class="p-5 flex flex-row flex-auto container z-1">
+    <div class="p-5 container z-1 header">
       <div class="text-900 font-medium text-xl mb-3">
         <h1>Reports</h1>
         <p>Driver report list</p>
       </div>
-      <div class="p-5 flex flex-row flex-auto">
+      <div class="p-5 card-container">
         <pv-card>
           <template #content>
             <pv-table :value="reports">
@@ -52,5 +52,15 @@ export default {
   max-width: 1500px;
   margin-left: 10rem;
   color: #495057;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  margin-bottom: 20px; /* Espacio entre el encabezado y el pv-card */
+}
+
+.card-container {
+  align-self: center; /* Centrar el pv-card horizontalmente */
 }
 </style>
