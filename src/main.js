e7a6feaaa673f830907cc830121e804cbdb61db1
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-04-29 18:16:06 -0500

    fix: fix the sidebar public

--- src/main.js
+++ src/main.js
@@ -10,10 +10,14 @@ import 'primeicons/primeicons.css'
 import PrimeVue from "primevue/config";
 import router from "./router/index.js";
 import Card from "primevue/card";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
 
 const app = createApp(App);
 app.use(PrimeVue,{ripple:true})
     .use(router)
     .component('pv-card', Card)
+    .component('pv-sidebar',Sidebar)
+    .component('pv-avatar',Avatar)
 
 app.mount('#app')
--- src/public/pages/display-vehicles-carrier.component.vue
+++ src/public/pages/display-vehicles-carrier.component.vue
@@ -1,13 +1,15 @@
 <script>
 import {defineComponent} from "vue";
 import CyaVehiclesCarrier from "../../cya/components/cya-vehicles-carrier.component.vue";
+import SidebarPublic from "../components/sidebar.component.vue";
 
 export default defineComponent({
-  components: {CyaVehiclesCarrier}
+  components: {SidebarPublic, CyaVehiclesCarrier}
 })
 </script>
 
 <template>
+  <sidebar-public/>
   <cya-vehicles-carrier/>
 </template>
 
