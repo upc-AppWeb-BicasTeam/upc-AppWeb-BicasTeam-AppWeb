b1999109ff00ba96e8686994563e5bc9c5ba878c
MoisesD19 <moisesdonayre19@gmail.com>
2024-04-30 14:14:50 -0500

    feat: added view shipment component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import OrganizationShipmentComponent from "../public/pages/organization-shipment.component.vue";
+import ViewShipmentsComponent from "../public/pages/view-shipments.component.vue";
 
 const router  = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/organization', component: OrganizationShipmentComponent}
+        { path: '/organization', component: OrganizationShipmentComponent},
+        { path: '/organization/view', component: ViewShipmentsComponent},
     ]
 });
 export default router;
