916700e02285216a267c37e4fa8a4cac1d0f2241
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-04-30 22:34:37 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/upc-AppWeb-BicasTeam-AppWeb.github.io/",
+  base:"/upc-AppWeb-BicasTeam/upc-AppWeb-BicasTeam-AppWeb.github.io/",
   plugins: [vue()],
 })
